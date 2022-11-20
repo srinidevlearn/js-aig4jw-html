c209b2353f11bce1eeab0774685db9171723fa71
srinidevlearn <65352160+srinidevlearn@users.noreply.github.com>
2022-11-20 12:57:20 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>JS HTML Forms</h1>`;
 
 // forms
 
