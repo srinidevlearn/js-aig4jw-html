81698091e0b9b5e78aa16a6babcf880b98be25be
srinidevlearn <65352160+srinidevlearn@users.noreply.github.com>
2022-11-20 12:08:24 +0530

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// forms
+
+let temp = `<option value="">--Select--</option>`;
+Array(43)
+  .fill(0)
+  .map((i, ind) => {
+    temp += `<option value=${ind + 18}>${ind + 18}</option>`;
+  });
+
+document.getElementById('ageField').innerHTML = temp;
